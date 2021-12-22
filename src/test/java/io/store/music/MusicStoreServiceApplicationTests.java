d47d99e47c974bd18802485e6bb668704dd2505a
Subin C <schel@nets.eu>
2021-12-22 09:41:50 +0530

    first commit

---
+++ src/test/java/io/store/music/MusicStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.store.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
